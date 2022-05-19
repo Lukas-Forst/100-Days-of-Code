ccf34df8abba360382e875cafecd1af21e14acc7
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-05-19 23:17:19 +0200

    Day 9
    change in endLottery, constructor, Lottery_state

--- smartcontract-lottery/contracts/Lottery.sol
+++ smartcontract-lottery/contracts/Lottery.sol
@@ -2,24 +2,35 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract Lottery {
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAdress) public{
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAdress);
     }
 
-    function enter() public payable{
+    function enter() public payable {
         // $50 minimum
-        
-        players.push(msg.sender);
-
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(payable(msg.sender));
     }
+
     function getEntraceFee() public view returns (uint256){
         (, int price, ,,,) = ethUsdPriceFeed.latestRoundData;
         uint256 adjustedPrice = uint256(price) * 10**10; 
@@ -36,7 +47,10 @@ contract Lottery {
         lottery_state = LOTTERY_STATE.OPEN;
     }
     function endLottery() public onlyOwner{
-        uint(keccack256(abi.encodePacked(nonce, msg.sender, block.difficulty, block.timestamp, block.timestamp))) % players.length;
+        //uint(keccack256(abi.encodePacked(nonce, msg.sender, block.difficulty, block.timestamp, block.timestamp))) % players.length;
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        emit RequestedRandomness(requestId);
     }
 
 }
