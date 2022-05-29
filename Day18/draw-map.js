e7bc0b1766afaccc827142f398163b9e76d57d97
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-05-29 22:20:09 +0200

    fixed legend errors
    added interaction

--- Day18/draw-map.js
+++ Day18/draw-map.js
@@ -27,7 +27,7 @@ async function drawMap() {
     .fitWidth(dimensions.boundedWidth, sphere)
 
   const pathGenerator = d3.geoPath(projection)
-  const [[x0, y0], [x1,y1]] = pathGenerator.bounds(sphere)
+  const [[x0, y0], [x1, y1]] = pathGenerator.bounds(sphere)
 
   dimensions.boundedHeight = y1
   dimensions.height = dimensions.boundedHeight
@@ -40,19 +40,20 @@ async function drawMap() {
       .attr("height", dimensions.height)
 
   const bounds = wrapper.append("g")
-    .style("transform", `translate(${
-    dimensions.margin.left
-    }px, ${
-      dimensions.margin.top
-    }px)`)
+      .style("transform", `translate(${
+        dimensions.margin.left
+      }px, ${
+        dimensions.margin.top
+      }px)`)
   
-  const metricValues = Object.values(metricDataByCountry)
-  const metricValueExtent = d3.extent(metricValues)
+    const metricValues = Object.values(metricDataByCountry)
+    const metricValueExtent = d3.extent(metricValues)
+    const maxChange = d3.max([-metricValueExtent[0], metricValueExtent[1]])
+    const colorScale = d3.scaleLinear()
+        .domain([-maxChange, 0, maxChange])
+        .range(["indigo", "white", "darkgreen"])
 
-  const maxChange = d3.max([-metricValueExtent[0], metricValueExtent[1]])
-  const colorScale = d3.scaleLinear()
-    .domain([-maxChange,0, maxChange])
-    .range(["indigo", "white", "darkgreen"])
+  // Draw Data
 
   const earth = bounds.append("path")
     .attr("class", "earth")
@@ -60,6 +61,11 @@ async function drawMap() {
   
   //A graticule is a grid of latitudinal and longitudinal lines
   const graticuleJson = d3.geoGraticule10()
+  const graticule = bounds.append("path")
+      .attr("class", "graticule")
+      .attr("d", pathGenerator(graticuleJson))
+
+
   const countries = bounds.selectAll(".country")
     .data(countryShapes.features)
     .enter().append("path")
@@ -71,7 +77,7 @@ async function drawMap() {
         return colorScale(metricValue)
       })
 
-  const legendGroup = wrapper.append("g")
+    const legendGroup = wrapper.append("g")
       .attr("transform", `translate(${
         120
       },${
@@ -79,16 +85,17 @@ async function drawMap() {
         ? dimensions.boundedHeight - 30
         : dimensions.boundedHeight * 0.5
       })`)
+
   const legendTitle = legendGroup.append("text")
       .attr("y", -23)
       .attr("class", "legend-title")
       .text("Population growth")
-  
+
   const legendByline = legendGroup.append("text")
       .attr("y", -9)
       .attr("class", "legend-byline")
       .text("Percent change in 2017")
-  
+
   const defs = wrapper.append("defs")
   const legendGradientId = "legend-gradient"
   const gradient = defs.append("linearGradient")
@@ -98,8 +105,9 @@ async function drawMap() {
     .enter().append("stop")
       .attr("stop-color", d => d)
       .attr("offset", (d, i) => `${
-        i * 100 / 2 //2 is one less than the length of our array
+        i * 100 / 2 // 2 is one less than our array's length
       }%`)
+
   const legendWidth = 120
   const legendHeight = 16
   const legendGradient = legendGroup.append("rect")
@@ -107,6 +115,7 @@ async function drawMap() {
       .attr("height", legendHeight)
       .attr("width", legendWidth)
       .style("fill", `url(#${legendGradientId})`)
+
   const legendValueRight = legendGroup.append("text")
       .attr("class", "legend-value")
       .attr("x", legendWidth / 2 + 10)
@@ -121,18 +130,49 @@ async function drawMap() {
       .style("text-anchor", "end")
   
 
-  navigator.geolocation.getCurrentPosition(myPosition => {
-           const [x, y] = projection([
-           myPosition.coords.longitude,
-           myPosition.coords.latitude
-           ])
-  const myLocation = bounds.append("circle")
-           .attr("class", "my-location")
-           .attr("cx", x)
-           .attr("cy", y)
-           .attr("r", 0)
-           .transition().duration(500)
-           .attr("r", 10)
-           })
+      navigator.geolocation.getCurrentPosition(myPosition => {
+        const [x, y] = projection([
+          myPosition.coords.longitude,
+          myPosition.coords.latitude
+        ])
+        const myLocation = bounds.append("circle")
+            .attr("class", "my-location")
+            .attr("cx", x)
+            .attr("cy", y)
+            .attr("r", 0)
+            .transition().duration(500)
+            .attr("r", 10)
+      })
+
+  countries.on("mouseenter", onMouseEnter)
+      .on("mouseleave", onMouseLeave)
+
+  const tooltip = d3.select("#tooltip")
+  function onMouseEnter(datum) {
+    tooltip.style("opacity", 1)
+
+    const metricValue = metricDataByCountry[countryIdAccessor(datum)]
+
+    tooltip.select("#country")
+        .text(countryNameAccessor(datum))
+
+    tooltip.select("#value")
+        .text(`${d3.format(",.2f")(metricValue || 0)}%`)
+
+    const [centerX, centerY] = pathGenerator.centroid(datum)
+
+    const x = centerX + dimensions.margin.left
+    const y = centerY + dimensions.margin.top
+
+    tooltip.style("transform", `translate(`
+      + `calc( -50% + ${x}px),`
+      + `calc(-100% + ${y}px)`
+      + `)`)
+
+  }
+
+  function onMouseLeave() {
+    tooltip.style("opacity", 0)
+  }
 }
 drawMap()
