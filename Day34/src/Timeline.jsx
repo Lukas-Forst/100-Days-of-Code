2755648a209d07a72e81fc611ab33d7e64e10cba
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-06-15 23:37:45 +0200

    Day 37
    update Timeline

--- Day34/src/Timeline.jsx
+++ Day34/src/Timeline.jsx
@@ -4,7 +4,7 @@ import * as d3 from "d3"
 
 import Chart from "./Chart/Chart"
 import Line from "./Chart/Line"
-import Axis from "./Chart/Axis-naive"
+import Axis from "./Chart/Axis"
 import { useChartDimensions, accessorPropsType } from "./Chart/utils";
 
 const formatDate = d3.timeFormat("%-b %-d")
