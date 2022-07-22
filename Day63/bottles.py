6fcb86bb9af54dc77872750d75c6895e0abf9ea0
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-07-22 22:58:51 +0200

    Day74
    finished bottles program

--- Day63/bottles.py
+++ Day63/bottles.py
@@ -34,20 +34,35 @@ def get_args():
 
 # --------------------------------------------------
 def bottle(num):
-  if num == 0:
-    print("No more bottles of beer on the wall!")
-  else:
-    if num == 1:
-      print(f"""{num} bottle of beer on the wall,\n{num} bottle of beer,\nTake one down, pass it around,""")
-    else:
-      print(f"""{num} bottles of beer on the wall,\n{num} bottles of beer,\nTake one down, pass it around,""")
-    
+  s1 = '' if num == 1 else 's'
+  s2 = '' if num-1 == 1 else 's'
+  num_next = 'No more' if num-1 == 0 else num-1
+  return '\n'.join([
+    f'{num} bottle{s1} of beer on the wall,', f'{num} bottle{s1} of beer,',
+'Take one down, pass it around,',f'{num_next} bottle{s2} of beer on the wall!'
+])
+
+
+
+def test_bottle():
+  """Test verse"""
+  last_verse = bottle(1)
+  assert last_verse == '\n'.join([
+'1 bottle of beer on the wall,', '1 bottle of beer,',
+'Take one down, pass it around,',
+'No more bottles of beer on the wall!',
+])
+  two_bottles = bottle(2)
+  assert two_bottles == '\n'.join(['2 bottles of beer on the wall,', '2 bottles of beer,',
+    'Take one down, pass it around,', '1 bottle of beer on the wall!'
+])
+
 def main():
     """Make a jazz noise here"""
     args = get_args()
     num = args.num
-    for i in range(num, -1, -1):
-      bottle(i)
+    print('\n\n'.join(map(bottle, range(args.num,0,-1))))
+
 
 
 # --------------------------------------------------
