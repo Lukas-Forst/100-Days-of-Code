6878650795f4b7a123f7d279a6b61865db230534
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-07-30 22:35:10 +0200

    Day81
    final version of twelve days

--- Day63/twelve_days.py
+++ Day63/twelve_days.py
@@ -8,6 +8,18 @@ Purpose: Rock the Casbah
 import argparse
 import sys
 
+day_one = '\n'.join([
+    'On the first day of Christmas,', 'My true love gave to me,',
+    'A partridge in a pear tree.'
+])
+
+day_two = '\n'.join([
+    'On the second day of Christmas,',
+    'My true love gave to me,',
+    'Two turtle doves,',
+    'And a partridge in a pear tree.'
+])
+
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
@@ -43,42 +55,54 @@ def get_args():
 # --------------------------------------------------
 def verse(day):
   gifts = {
-    1: "A partridge in a pear tree",
-    2:"Two turtle doves",
-    3 :"Three French hens",
-    4 :"Four calling birds",
-    5 :"Five gold rings",
-    6 :"Six geese a laying",
-    7 :"Seven swans a swimming",
-    8 :"Eight maids a milking",
-    9 :"Nine ladies dancing",
-    10 :"Ten lords a leaping",
-    11 :"Eleven pipers piping",
-    12 :"Twelve drummers drumming"
+    0: "A partridge in a pear tree",
+    1: "Two turtle doves",
+    2 :"Three French hens",
+    3 :"Four calling birds",
+    4 :"Five gold rings",
+    5 :"Six geese a laying",
+    6 :"Seven swans a swimming",
+    7 :"Eight maids a milking",
+    8 :"Nine ladies dancing",
+    9 :"Ten lords a leaping",
+    10 :"Eleven pipers piping",
+    11 :"Twelve drummers drumming"
   }
-  #print(day)
-  arr_t = []
-  ordinal = ['','first','second','third','fourth','fifth','sixth','seventh','eigth','nineth','tenth','eleven','twelve'] # something here!
+  
+  #day = day-1
+  ordinal = ['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth'] 
+  days = []
+  
+  
+  # something here!
   #day = str(day)
-  arr_t.append(f'On the {ordinal[day]} day of Christmas,')
-  arr_t.append('My true love gave to me,')
-  if day == 1:
-    return f"{gifts[day]}."
-  else:
-    for n in reversed(range(1, day+1)):
-      if n == 1:
-        val = gifts[n]
-        arr_t.append(f"And {val[0].lower()}{val[1:]}.")
-      else:
-        arr_t.append(f"{gifts[n]},")
-  return '\n'.join(arr_t)
-   
-
-
-
-
+  #print(f'On the {ordinal[day]} day of Christmas,')
+  #print('My true love gave to me,')
+  for i in range(0, day):
+    #print(i)
+    if i == 0:
+      #print(f"{i} is 0?")
+      arr = [f'On the {ordinal[i]} day of Christmas,','My true love gave to me,',f'{gifts[i]}.']
+      days.append('\n'.join(arr))
+    else:
+      #print("Test")
+      arr_t = [f"On the {ordinal[i]} day of Christmas,","My true love gave to me,"]
+      for n in reversed(range(0, i+1)):
+        
+        if n == 0:
+          val = gifts[n]
+          
+          arr_t.append(f"And {val[0].lower()}{val[1:]}.")
+        else:
+          arr_t.append(f"{gifts[n]},")
+      #arr = '\n'.join(arr_t)
+      days.append('\n'.join(arr_t))
+      #days.append(arr_t)
+  #print(days)
+  return '\n\n'.join(days)
+    
 def test_verse():
-  """Test verse"""
+  """Test verse
   assert verse(1) == '\n'.join([
   'On the first day of Christmas,', 'My true love gave to me,',
   'A partridge in a pear tree.'
@@ -86,23 +110,21 @@ def test_verse():
   assert verse(2) == '\n'.join([
   'On the second day of Christmas,', 'My true love gave to me,',
   'Two turtle doves,', 'And a partridge in a pear tree.'
-  ])
+  ])"""
+  assert verse(2) == '\n\n'.join([day_one, day_two])
 
 def main():
     """Make a jazz noise here"""
     args = get_args()
     num = args.num
     file_arg = args.output
+    #print(file_arg)
     #if file_arg:
-        #with open(file_arg, "w") as file:
-          #print(file_arg)
-          #text = '\n\n'.join(map(verse, range(args.num,0,-1)))
-          #file.write(text)
-    for day in range(1, num+1):
-      print(verse(day))
-    #else:
-        #print(text.upper())
-        #print('\n\n'.join(map(verse, range(args.num,0,-1))))
+    #  with open(file_arg) as file:
+    #    file.write(verse(num))
+    print(verse(num), file=file_arg)
+
+
 
 
 
