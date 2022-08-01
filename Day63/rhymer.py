73975d8f81061517fab610ec1faf95000ec9b9ec
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-08-01 22:25:14 +0200

    Day83
    finished rhymer version of tiny python projects book

--- Day63/rhymer.py
+++ Day63/rhymer.py
@@ -20,21 +20,30 @@ def get_args():
     return parser.parse_args()
 def stemmer(word):
   """Return leading consonants (if any), and 'stem' of word"""
-  consonants = ''.join([c for c in s.ascii_lowercase if c not in 'aeiou'])
-  pattern = '[' + consonants + ']'
-  match = re.match(f'([{consonants}]+)(.+)', 'chair')
-  print(match)
+  
   #print(('aeiou' in word.lower()))
   if word == '':
     return ('', '')
-  vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-  if any(char in vowels for char in word) == False:
-    return (word, '')
+  #vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+  #if any(char in vowels for char in word) == False:
+    #print(word)
+    #return (word.lower(), '')
   elif word.isdigit():
     return (word, '')
   else:
-    matcher = re.match('^[^aeiou]+',word)
-    return (word[:matcher.end(0)], word[matcher.end(0):])
+    #print(word)
+    consonants = ''.join([c for c in s.ascii_lowercase if c not in 'aeiou'])
+    pattern = '[' + consonants + ']'
+    matcher_2 = re.match(f'([{consonants}]+)?([aeiou])(.*)', word.lower(), re.IGNORECASE)#
+    #print(matcher_2.group(0),matcher_2.group(1), matcher_2.group(2))
+    if matcher_2:
+      p1 = matcher_2.group(1) or ''
+      p2 = matcher_2.group(2) or ''
+      p3 = matcher_2.group(3) or ''
+      return (p1, p2 + p3)
+    else:
+      return (word, '')
+
 
 def test_stemmer():
   """ Test stemmer """
@@ -45,13 +54,34 @@ def test_stemmer():
   assert stemmer('RDNZL') == ('rdnzl', '')
   assert stemmer('123') == ('123', '')
 
+
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
 
     args = get_args()
     str_arg = args.word
-    print(stemmer(str_arg))
+    tup = stemmer(str_arg)
+    consonants = ''.join([c for c in s.ascii_lowercase if c not in 'aeiou'])
+    pattern = '[' + consonants + ']'
+    match = re.match(f'([{consonants}]+)?(.+)', 'rdnzl')
+    consonants_a = ["bl", "br", "ch", "cl", "cr", "dr", "fl", "fr", "gl", "gr", "pl",         "pr", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st","sw", "th", "tr", "tw", "thw",       "wh", "wr", "sch", "scr", "shr", "sph", "spl", "spr", "squ", "str", "thr"]
+    consonants_n = [c for c in s.ascii_lowercase if c not in 'aeiou']
+    consonants_n.extend(consonants_a)
+    #print(consonants_n)
+    consonants_n.sort()
+    #print(tup)
+    if tup[0] != '' and tup[1] != '':
+      for i in consonants_n:
+        if tup[0] != '':
+          if i+tup[1] != str_arg.lower():
+            print(i+tup[1])
+    elif tup[0] == '' and tup[1] != '':
+      for i in consonants_n:
+        print(i+tup[1])
+    else:
+      print(f'Cannot rhyme "{str_arg}"')
+    #print(match.groups())
 
     
 
