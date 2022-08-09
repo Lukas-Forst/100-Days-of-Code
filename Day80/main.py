8a3dae7d18e7f97f7e09ec0698e0cd62731a91d0
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-08-09 22:30:31 +0200

    Day89
    not really much today totally forgot the upload.
    Added some polar_scatter charts but will update the dashboard

--- Day80/main.py
+++ Day80/main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-
+import matplotlib.pyplot as plt
+import plotly.express as px
 st.title('Comparison chart')
 
 DATE_COLUMN = 'date/time'
@@ -51,3 +52,42 @@ st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
 st.text(add_selectbox)
 hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+N = 150
+r = 2 * np.random.rand(N)
+theta = 2 * np.pi * np.random.rand(N)
+area = 200 * r**2
+colors = theta
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='polar')
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+
+ax.set_thetamin(45)
+ax.set_thetamax(135)
+st.pyplot(fig)
+
+col1, col2 = st.columns(2)
+
+
+fig2 = px.scatter_polar(r=range(45, 90, 5), theta=range(0, 45, 5),
+                       range_theta=[0,90], start_angle=90)
+col1.plotly_chart(fig2)
+col1.write("Fig2")
+fig3 = px.scatter_polar(r=range(0, 90, 10), theta=range(90, 180, 10),
+                        range_theta=[0,90], start_angle=180, direction='clockwise')
+col2.plotly_chart(fig3, use_container_width=False)
+col2.write("Fig3")
+fig4 = px.scatter_polar(r=range(0, 90, 10), theta=range(180, 270, 10),
+                        range_theta=[90, 180], start_angle=270, direction='counterclockwise')
+col1.plotly_chart(fig4, use_container_width=False)
+col1.write("Fig4")
+fig5 = px.scatter_polar(r=range(0, 90, 10), theta=range(270, 360, 10),
+                        range_theta=[180, 270], start_angle=270, direction='counterclockwise')
+col2.plotly_chart(fig5, use_container_width=False)
+col2.write("Fig5")
+az = [10 for i in range(0,100)]
+range_n = [i for i in range(0,100)]
+st.write("PSR")
+fig6 = px.scatter_polar(r=range_n, theta=az,
+                        range_theta=[0,90], start_angle=0, direction='counterclockwise')
+st.plotly_chart(fig6, use_container_width=False)
