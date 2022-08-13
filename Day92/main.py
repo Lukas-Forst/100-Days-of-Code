7277164c7f88f4ee3b86204dbce3d373a41cba8d
Lukas <12475275+Lukas-Forst@users.noreply.github.com>
2022-08-13 22:29:14 +0200

    Day93
    added heatmap, and other charts for a better overview

--- Day92/main.py
+++ Day92/main.py
@@ -1,74 +1,62 @@
 import streamlit as st
 import pandas as pd
 #import seaborn as sns
-import json
-import requests
+import plotly.express as px
 import matplotlib.pyplot as plt
-token = ""
-database_id = ""
+import datetime
+import plotly.graph_objects as go
 
 st.write("""
 #  Notion Dashboard
 Viz of different Notion dbs
 """)
-def mapNotionResult(result):
-  # you can print result here and check the format of the answer.
-    id_ = result['id']
-    properties = result['properties']
-    #print(result)
-    Sleep = properties['😴7+ hrs Sleep']['checkbox']
-    Journaling = properties['✍🏼Journaling']['checkbox']
-    Coden = properties['Coden']['checkbox']
-    dehnen = properties['Dehnen']['checkbox']
-    Training = properties['Training']['checkbox']
-    SAVERS = properties['SAVERS']['checkbox']
-    # check for none
-    if properties['Date']['date'] != None:
-        Date = properties['Date']['date']['start']
-    else:
-        Date = properties['Date']['date']#['start']
-    return {
-    'Date': Date,
-    'Sleep': Sleep,
-    'Journaling': Journaling,
-    'Coden': Coden,
-    'dehnen': dehnen,
-    "Training": Training,
-    "SAVERS": SAVERS
-    }
 
-def getData():
-    url = f'https://api.notion.com/v1/databases/{database_id}/query'
-
-    r = requests.post(url, headers={
-    "Authorization": f"Bearer {token}",
-    "Notion-Version": "2021-08-16"
-  })
-
-    result_dict = r.json()
-    list_result = result_dict['results']
-
-    data = []
-
-    for day in list_result:
-
-        _dict = mapNotionResult(day)
-        data.append(_dict)
-
-    return data
-
-habits = getData()
-habits = [habit for habit in habits if habit['Date']!=None]
-df = pd.DataFrame(habits)
+df = pd.read_csv('habits.csv')
 
 
 for index, row in df.iterrows():
     df.loc[index,'count'] = int(row['Sleep'])+int(row['Journaling'])+ int(row['Coden'])+ int(row['dehnen'])+int(row['Training'])+int(row['SAVERS'])
-fig, ax = plt.subplots()
-ax.bar(df.Date.values, df['count'].values,width=0.5)
-st.pyplot(fig)
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+df_week = df.groupby(by="Week")['count'].sum()
+st.write(df_week.head())
+
+
+fig = px.line(df_week, y=df_week.values, x=df_week.index.values, title='Life expectancy in Canada')
+st.plotly_chart(fig)
+
+####
+# Current Week stats
+####
+
+
+curr_week = datetime.datetime.now().isocalendar().week
+curr_last_week = curr_week-1 if curr_week != 1 else 53
+df_curr_week = df[df['Week'] == curr_week]
+df_last_week  = df[df['Week'] == curr_last_week]
+st.write("Current Week metrics")
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+delta_sleep = df_curr_week['Sleep'].sum() - df_last_week['Sleep'].sum()
+print(delta_sleep)
+cols = ['Sleep', 'Journaling', 'Coden', 'dehnen','Training','SAVERS']
+for colum in range(1,len(cols)+1):
+    val = colum
+    current_column = cols[colum-1]
+    delta = df_curr_week[current_column].sum() - df_last_week[current_column].sum()
+    print(current_column, delta, df_curr_week[current_column].sum() , df_last_week[current_column].sum())
+    if val % 3 == 0:
+        col1.metric(current_column,df_curr_week[current_column].sum(), f"{int(delta)} change")
+    if val % 3 == 1:
+        col2.metric(current_column, df_curr_week[current_column].sum(), f"{int(delta)} change")
+    if val % 3 == 2:
+        col3.metric(current_column, df_curr_week[current_column].sum(), f"{int(delta)} change")
+
+st.write()
+st.write()
+st.write("# Heatmap weekly overview of habits")
+
+fig_heat = go.Figure(data=go.Heatmap(
+                   z=df['count'].values,
+                   x=df['Weekday'].values,
+                   y=df['Week'].values,
+                   hoverongaps = False))
+st.plotly_chart(fig_heat)
